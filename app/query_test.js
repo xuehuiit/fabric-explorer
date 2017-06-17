1a239ba39b3035dd49229f1a225885bd8df17b64
gcc2ge <1414786333@qq.com>
2017-06-17 19:44:15 +0800

    init

--- app/query_test.js
+++ app/query_test.js
@@ -16,7 +16,9 @@ helper.getAdminUser('org1').then(user =>{
 */
 
 
-/*query.getBlockByNumber('peer1',4,'admin','org1').then(response_payloads=>{
+/*query.getBlockByNumber('peer1','mychannel',0,'admin','org1').then(response_payloads=>{
+    console.info("==========================================")
+    // console.info(JSON.stringify(response_payloads.data.data[0]))
     console.info(JSON.stringify(response_payloads))
 }).catch(err =>{
     console.info(err)
@@ -36,17 +38,17 @@ query.getTransactionByID('peer1','63843009a8114e3a6fb68f510f638b5f6a7e1448759c03
     console.info(err)
 })*/
 
-query.getChainInfo('peer1','mychannel','admin','org1').then(response_payloads=>{
+/*query.getChainInfo('peer1','mychannel','admin','org1').then(response_payloads=>{
     console.info(response_payloads)
 }).catch(err=>{
     console.info(err)
-})
+})*/
 
-/*query.getInstalledChaincodes('peer1','installed','admin','org1').then(response=>{
+query.getInstalledChaincodes('peer1','mychannel','installed','admin','org1').then(response=>{
     console.info(response)
 }).catch(err=>{
     console.info(err)
-})*/
+})
 
 /*
 query.getChannels('peer1','admin','org1').then(response=>{
@@ -55,3 +57,24 @@ query.getChannels('peer1','admin','org1').then(response=>{
     console.info(err)
 })
 */
+
+/*function getTxCount(){
+    return Promise.all([
+            query.getBlockByNumber('peer1','mychannel',0,'admin','org1'),
+            query.getBlockByNumber('peer1','mychannel',1,'admin','org1'),
+            query.getBlockByNumber('peer1','mychannel',2,'admin','org1'),
+            query.getBlockByNumber('peer1','mychannel',3,'admin','org1')
+        ]
+    ).then(datas=>{
+        let txcount=0
+        datas.forEach((k,v) =>{
+            txcount+=k.data.data.length
+        })
+        return Promise.resolve(txcount)
+    }).catch(err=>{
+        console.info(err)
+    })
+}
+getTxCount().then(txcount=>{
+    console.info(txcount)
+})*/
