69324d4ab7aeca42ac8dde02aa48cb1b5386ce27
gcc2ge <1414786333@qq.com>
2017-06-17 11:57:54 +0800

    init

--- main.js
+++ main.js
@@ -23,6 +23,19 @@ app.set('views', __dirname + '/views');
 
 // =======================   控制器绑定  ===================
 
+/**
+ *  /channels
+ *  /channels/:channelName
+ *  /channels/:channelName/block/:blockNum
+ *  /channels/:channelName/block/:blockHash
+ *  /channels/:channelName/block/:blockNum/:txHash
+ *  /channels/:channelName/chaincodes
+ *
+ *  /orgs
+ *  /orgs/peers
+ *
+ *
+ */
 
 //首页
 app.get("/", function(req, res) {
@@ -48,48 +61,56 @@ app.get("/orgs", function(req, res) {
     });
 });
 
-//账本列表
+app.get("/orgs/peers", function(req, res) {
+    res.render('orgs.ejs', {
+        name: 'tinyphp',item_index_orgs:'1'
+    });
+});
+
+//channel列表
 app.get("/channels", function(req, res) {
     res.render('channels.ejs', {
         name: 'tinyphp',item_index_channels:'1'
     });
 });
 
-//账本详情
-app.get("/channel_detail", function(req, res) {
+//channel详情
+app.get("/channels/:channelName", function(req, res) {
     res.render('channel_detail.ejs', {
         name: 'tinyphp',item_index_channels:'1'
     });
 });
 
-//节点列表
-app.get("/peers", function(req, res) {
+//区块列表
+app.get("/channels/:channelName/block/:blockNum", function(req, res) {
     res.render('peers.ejs', {
         name: 'tinyphp',item_index_peers:'1'
     });
 });
 
-//节点详情
-app.get("/peer_detail", function(req, res) {
+//区块详情
+app.get("/channels/:channelName/block/:blockHash", function(req, res) {
     res.render('peer_detail.ejs', {
         name: 'tinyphp',item_index_peers:'1'
     });
 });
 
-//区块详情
-app.get("/block_detail", function(req, res) {
+//交易详情
+app.get("/channels/:channelName/block/:blockNum/:txHash", function(req, res) {
     res.render('block_detail.ejs', {
         name: 'tinyphp',item_index_peers:'1'
     });
 });
 
 
-//交易详情
-app.get("/trans_detail", function(req, res) {
+//
+app.get("/channels/:channelName/chaincodes", function(req, res) {
     res.render('trans_detail.ejs', {
         name: 'tinyphp',item_index_peers:'1'
     });
 });
+
+
 
 
 
