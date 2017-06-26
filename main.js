c06ab03987cbccfebe18e1a4273925c3c67f51c5
gcc2ge <1414786333@qq.com>
2017-06-26 10:11:11 +0800

    init

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var app = express();
 var http= require('http').Server(app);
 var io = require('socket.io')(http);
 
-require('./socket/websocketserver.js').init(app)
+require('./socket/websocketserver.js').init(io)
 
 var timer=require('./timer/timer.js')
 timer.start(io)
