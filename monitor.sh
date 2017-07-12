66a90b43925560410b66d992684f1ff4d7169b34
gcc2ge <1414786333@qq.com>
2017-07-12 11:31:42 +0800

    monitor script

--- monitor.sh
+++ monitor.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ];do
     sleep 10
-    process_num=$(ps -elf | grep -v grep | grep node | wc -l)
+    process_num=$(ps -elf | grep -v grep | grep main.js | wc -l)
     if [ ${process_num} -eq 0 ];then
         ./start.sh
     fi
